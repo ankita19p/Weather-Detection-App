28e7c401e05f0d5089ccc666cfd95171c2c135aa
Ankita Patra <95522272+ankita19p@users.noreply.github.com>
2024-09-16 20:28:55 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+//import UseState from ". /component/Hooks/useState";
+//import UseEffect from "./component/Hooks/useEffect";
+//import Resturant from "./component/Basics/Resturant"; 
+//import UseReducer from "./component/Hooks/useReducer";
+//import Todo from "./component/todoreact/todo";
+import WeatherApp from "./component/weather/temp";  
+const  App =() => {
+  return (
+    <>
+   <WeatherApp  />
+    </>
+  );
+};
+
+
+
+export default App;
